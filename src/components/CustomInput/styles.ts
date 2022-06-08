8c57af6ae70f3eefd675fda4410d2c5ccda67e60
Anderson Souza <andersonszdc@gmail.com>
2022-06-08 11:19:43 -0300

    add focus and hover in custom input

--- src/components/CustomInput/styles.ts
+++ src/components/CustomInput/styles.ts
@@ -9,12 +9,23 @@ export const StyledInput = styled.div`
     display: flex;
     gap: 8px;
 
-    border: 2px solid #00E3E3;
+    border: 2px solid #00e3e3;
     border-radius: 8px;
     padding: 12px;
 
     font-size: 16px;
     font-weight: 400;
+    transition: 0.2s ease-in;
+
+    :hover {
+      border-color: #e1e1e1;
+      box-shadow: 0px 0px 0px 6px rgba(0, 227, 227, 0.2);
+    }
+
+    :focus-within {
+      border-color: #00e3e3;
+      box-shadow: 0px 0px 0px 6px rgba(0, 227, 227, 0.2);
+    }
   }
 
   input {
