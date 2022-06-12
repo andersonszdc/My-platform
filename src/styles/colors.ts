0bd987e6e26bbaf85ed83f26f183a2124e9d185b
Anderson Souza <andersonszdc@gmail.com>
2022-06-12 12:57:27 -0300

    created colors system

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+import { ThemedStyledProps } from "styled-components";
+
+const theme = {
+  white: "hsl(330, 6%, 93%)",
+  black: "hsl(240, 100%, 5%)",
+  blue: "hsl(180, 100%, 45%)",
+  pink: "hsl(330, 100%, 64%)",
+  purple: "hsl(260, 71%, 15%)",
+  grayDark: "hsl(331, 3%, 26%)",
+  grayLight: "hsl(0, 0%, 88%)"
+};
+
+export default theme;
